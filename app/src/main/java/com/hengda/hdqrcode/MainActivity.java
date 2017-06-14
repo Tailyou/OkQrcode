78d500f4f6efe361ab73a6bd2291fce8793989de
tailyou <tailyou@163.com>
2017-06-14 11:32:07 +0800

    20170612

--- app/src/main/java/com/hengda/hdqrcode/MainActivity.java
+++ app/src/main/java/com/hengda/hdqrcode/MainActivity.java
@@ -1,18 +1,22 @@
 package com.hengda.hdqrcode;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.zxing.Result;
+import com.google.zxing.client.result.ParsedResult;
+import com.google.zxing.client.result.ParsedResultType;
+import com.google.zxing.client.result.TextParsedResult;
+import com.google.zxing.client.result.URIParsedResult;
 import com.hengda.zwf.hdscanner.ScanActivity;
 import com.hengda.zwf.hdscanner.ScanConfig;
 import com.hengda.zwf.hdscanner.ScanConfigBuilder;
-import com.mylhyl.zxing.scanner.common.Intents;
+import com.mylhyl.zxing.scanner.OnScannerCompletionListener;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
@@ -41,20 +45,37 @@ public class MainActivity extends AppCompatActivity {
                 .setTitle(R.string.title_scan)
                 .setScanTip(R.string.qrcode_scan_tip)
                 .create();
-        ScanActivity.gotoActivity(MainActivity.this, scanConfig);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != Activity.RESULT_CANCELED && resultCode == Activity.RESULT_OK) {
-            if (requestCode == ScanActivity.REQUEST_CODE_SCANNER) {
-                if (data != null) {
-                    String stringExtra = data.getStringExtra(Intents.Scan.RESULT);
-                    tvHello.setText(stringExtra);
+        OnScannerCompletionListener listener = new OnScannerCompletionListener() {
+            @Override
+            public void OnScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode) {
+                ParsedResultType type = parsedResult.getType();
+                switch (type) {
+                    case ADDRESSBOOK:
+                        break;
+                    case PRODUCT:
+                        break;
+                    case ISBN:
+                        break;
+                    case URI:
+                        URIParsedResult uri = (URIParsedResult) parsedResult;
+                        tvHello.setText(uri.getURI());
+                        ScanActivity.sInstance.finish();
+                        break;
+                    case TEXT:
+                        TextParsedResult textParsedResult = (TextParsedResult) parsedResult;
+                        tvHello.setText(textParsedResult.getText());
+                        ScanActivity.sInstance.finish();
+                        break;
+                    case GEO:
+                        break;
+                    case TEL:
+                        break;
+                    case SMS:
+                        break;
                 }
             }
-        }
+        };
+        ScanActivity.gotoActivity(MainActivity.this, scanConfig, listener);
     }
 
     @Override
--- library/src/main/java/com/hengda/zwf/hdscanner/ScanActivity.java
+++ library/src/main/java/com/hengda/zwf/hdscanner/ScanActivity.java
@@ -2,22 +2,14 @@ package com.hengda.zwf.hdscanner;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.google.zxing.Result;
-import com.google.zxing.client.result.ParsedResult;
-import com.google.zxing.client.result.ParsedResultType;
-import com.google.zxing.client.result.TextParsedResult;
-import com.google.zxing.client.result.URIParsedResult;
 import com.mylhyl.zxing.scanner.OnScannerCompletionListener;
 import com.mylhyl.zxing.scanner.ScannerView;
 import com.mylhyl.zxing.scanner.common.Intents;
@@ -28,31 +20,39 @@ import com.mylhyl.zxing.scanner.common.Intents;
  * @author 祝文飞（Tailyou）
  * @time 2017/4/27 17:22
  */
-public class ScanActivity extends AppCompatActivity implements OnScannerCompletionListener {
+public class ScanActivity extends AppCompatActivity {
 
     RelativeLayout toolbar;
     TextView tvTitle;
     ImageView ivBack;
     ScannerView scannerView;
-    public static final int REQUEST_CODE_SCANNER = 188;
     public static final String SCAN_CONFIG = "SCAN_CONFIG";
-    public static final String TAG = ScanActivity.class.getSimpleName();
-    private Result mLastResult;
+    public static ScanActivity sInstance;
+    private static OnScannerCompletionListener scannerCompletionListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        sInstance = this;
+        ScanConfig scanConfig = getIntent().getParcelableExtra(SCAN_CONFIG);
 
         ivBack = (ImageView) findViewById(R.id.ivBack);
-        scannerView = (ScannerView) findViewById(R.id.scanner_view);
-        toolbar = (RelativeLayout) findViewById(R.id.toolbar);
-        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
-        ScanConfig scanConfig = getIntent().getParcelableExtra(SCAN_CONFIG);
-        tvTitle.setText(scanConfig.getTitle());
+        toolbar = (RelativeLayout) findViewById(R.id.toolbar);
         toolbar.setBackgroundColor(scanConfig.getToolbarColor());
         StatusBarUtil.setColorNoTranslucent(ScanActivity.this, scanConfig.getToolbarColor());
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvTitle.setText(scanConfig.getTitle());
+
+        scannerView = (ScannerView) findViewById(R.id.scanner_view);
         scannerView.setLaserFrameBoundColor(scanConfig.getLaserColor());
         scannerView.setLaserColor(scanConfig.getLaserColor());
         scannerView.setMediaResId(scanConfig.getMediaResId());
@@ -61,19 +61,13 @@ public class ScanActivity extends AppCompatActivity implements OnScannerCompleti
         scannerView.setLaserFrameCornerLength(scanConfig.getFrameCornerLength());
         scannerView.setLaserLineHeight(scanConfig.getLaserLineHeight());
         scannerView.setDrawText(getString(scanConfig.getScanTip()), 16, Color.WHITE, true, 28);
-        scannerView.setOnScannerCompletionListener(this);
-        ivBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        scannerView.setOnScannerCompletionListener(scannerCompletionListener);
+
     }
 
     @Override
     protected void onResume() {
         scannerView.onResume();
-        resetStatusView();
         super.onResume();
     }
 
@@ -83,74 +77,10 @@ public class ScanActivity extends AppCompatActivity implements OnScannerCompleti
         super.onPause();
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-                if (mLastResult != null) {
-                    restartPreviewAfterDelay(0L);
-                    return true;
-                }
-                break;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    private void restartPreviewAfterDelay(long delayMS) {
-        scannerView.restartPreviewAfterDelay(delayMS);
-        resetStatusView();
-    }
-
-    private void resetStatusView() {
-        mLastResult = null;
-    }
-
-    @Override
-    public void OnScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode) {
-        if (rawResult == null) {
-            Log.d(TAG, "qrcode error");
-            finish();
-            return;
-        }
-        ParsedResultType type = parsedResult.getType();
-        switch (type) {
-            case ADDRESSBOOK:
-                break;
-            case PRODUCT:
-                break;
-            case ISBN:
-                break;
-            case URI:
-                URIParsedResult uri = (URIParsedResult) parsedResult;
-                Log.i(TAG, "uri: " + uri.getURI());
-                scanSuccess(uri.getURI());
-                break;
-            case TEXT:
-                TextParsedResult textParsedResult = (TextParsedResult) parsedResult;
-                Log.i(TAG, "text: " + textParsedResult.getText());
-                scanSuccess(textParsedResult.getText());
-                break;
-            case GEO:
-                break;
-            case TEL:
-                break;
-            case SMS:
-                break;
-        }
-    }
-
-    private void scanSuccess(String text) {
-        Intent intent = new Intent();
-        Bundle bundle = new Bundle();
-        bundle.putString(Intents.Scan.RESULT, text);
-        intent.putExtras(bundle);
-        setResult(RESULT_OK, intent);
-        finish();
-    }
-
-    public static void gotoActivity(Activity activity, ScanConfig scanConfig) {
+    public static void gotoActivity(Activity activity, ScanConfig scanConfig, OnScannerCompletionListener listener) {
         Intent intent = new Intent(Intents.Scan.ACTION).putExtra(SCAN_CONFIG, scanConfig);
-        activity.startActivityForResult(intent, REQUEST_CODE_SCANNER);
+        activity.startActivity(intent);
+        scannerCompletionListener = listener;
     }
 
 }
